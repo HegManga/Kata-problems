57a350ca211a4cbf05a5cab1d90d38af1d38ed4a
HegManga <28701547+HegManga@users.noreply.github.com>
2019-06-28 15:06:49 +0200

    Create Are_they_the_"same"?.c

---
+++ C/6kyu/Are_they_the_"same"?.c
@@ -0,0 +1,60 @@
+/*
+Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+Examples
+Valid arrays
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+Invalid arrays
+
+If we change the first number to something else, comp may not return true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+Remarks
+
+a or b might be [] (all languages except R, Shell). a or b might be nil or null or None or nothing (except in Haskell, Elixir, C++, Rust, R, Shell, PureScript).
+
+If a or b are nil (or null or None), the problem doesn't make sense so return false.
+
+If a or b are empty then the result is self-evident.
+
+a or b are empty or not empty lists.
+*/
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+int search_linear(int *a, size_t n, int x){
+    int i;
+    for( i =0; i<n && (a[i] != x); ++i); 
+    if(i>=n) return -1;
+    else return i;
+}
+
+bool comp(const int *a, const int *b, size_t n)
+{   int *c = malloc(n*sizeof(int));
+    int index;
+    
+    for(int i =0; i<n; ++i) c[i] = a[i]*a[i];
+    
+    for(int i =0; i<n; ++i){
+      index = search_linear(c,n,b[i]);
+      if(index == -1) return false;
+      else c[index] = -1;
+    }
+    return true;
+}
